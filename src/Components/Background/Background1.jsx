b75dd4ad201b85629d5aa8ffa4d4e642e7a909fd
AakashR11111001111 <aakash04721202019@msi-ggsip.org>
2025-03-19 22:25:10 +0530

    bug fixed

---
+++ src/Components/Background/Background1.jsx
@@ -0,0 +1,11 @@
+import styles from './Background1.module.css';
+const Background1 = () => {
+    return (
+        <div className={styles.bg}>
+           
+        </div>
+    );
+
+}
+
+export default Background1;
--- src/Components/Landing Page/Landing.jsx
+++ src/Components/Landing Page/Landing.jsx
@@ -1,11 +1,12 @@
-import Background from "../background/Background";
+
+import Background1 from "../Background/Background1";
 import Header from "../Header/Header";
 import Hero from "../Hero/Hero";
 
 const Landing  = ( {onGetStartedClick}) => {
     return (
         <>
-            <Background />
+            <Background1 />
             <Header />
             <Hero onGetStartedClick={onGetStartedClick} />
         </>
